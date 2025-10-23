d7cde4d50d5a81a0bea10c205b0cfb329589c91b
parsafaramarzi <pfaramazi@gmail.com>
2025-10-23 22:52:52 +0330

    small improvement

--- LogisticRegression Bank Customer.py
+++ LogisticRegression Bank Customer.py
@@ -16,7 +16,7 @@ y = data["churn"]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-model = LogisticRegression()
+model = LogisticRegression(max_iter=1000)
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
